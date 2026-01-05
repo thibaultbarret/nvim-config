bb9be33195db90f50e962b10756ff2ee982e5423
Thibault Barret <thibault_barret@hotmail.fr>
2026-01-05 11:48:54 +0100

    Updates

--- lua/plugins/rainbow-delimiters.lua
+++ lua/plugins/rainbow-delimiters.lua
@@ -1,24 +1,25 @@
 return {
+    -- "HiPhish/nvim-ts-rainbow2",
     "HiPhish/rainbow-delimiters.nvim",
-    config = function()
-        local rainbow_delimiters = require("rainbow-delimiters")
-
-        vim.g.rainbow_delimiters = {
-            strategy = {
-                [""] = rainbow_delimiters.strategy["global"],
-            },
-            query = {
-                [""] = "rainbow-delimiters",
-            },
-            highlight = {
-                "RainbowDelimiterRed",
-                "RainbowDelimiterYellow",
-                "RainbowDelimiterBlue",
-                "RainbowDelimiterOrange",
-                "RainbowDelimiterGreen",
-                "RainbowDelimiterViolet",
-                "RainbowDelimiterCyan",
-            },
-        }
-    end,
+    -- config = function()
+    --     local rainbow_delimiters = require("rainbow-delimiters")
+    --
+    --     vim.g.rainbow_delimiters = {
+    --         strategy = {
+    --             [""] = rainbow_delimiters.strategy["global"],
+    --         },
+    --         query = {
+    --             [""] = "rainbow-delimiters",
+    --         },
+    --         highlight = {
+    --             "RainbowDelimiterRed",
+    --             "RainbowDelimiterYellow",
+    --             "RainbowDelimiterBlue",
+    --             "RainbowDelimiterOrange",
+    --             "RainbowDelimiterGreen",
+    --             "RainbowDelimiterViolet",
+    --             "RainbowDelimiterCyan",
+    --         },
+    --     }
+    -- end,
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
     build = ":TSUpdate",
     config = function()
         require("nvim-treesitter.configs").setup({
-            highlight = { enable = true, disable = {} },
+            highlight = { enable = true, additional_vim_regex_highlighting = true },
             indent = { enable = true },
             ensure_installed = {
                 "bash",
@@ -20,6 +20,17 @@ return {
                 "json",
                 -- "biblatex",
             },
+            -- rainbow = {
+            -- enable = true,
+            -- list of languages you want to disable the plugin for
+            -- disable = { "jsx", "cpp" },
+            -- Which query to use for finding delimiters
+            -- query = "rainbow-parens",
+            -- -- Highlight the entire buffer all at once
+            -- strategy = require("ts-rainbow.strategy.global"),
+            -- -- Do not enable for files with more than n lines
+            -- max_file_lines = 3000,
+            -- },
         })
     end,
     vim.treesitter.language.register("cpp", "mfront"),
